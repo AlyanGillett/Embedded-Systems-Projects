1343acd52a77704d2a1ac72e84ad64fd357c5b5d
AlyanGillett <83925469+AlyanGillett@users.noreply.github.com>
2023-09-23 14:45:26 -0700

    Update LED_PUSH_BUTTON_DISPLAY.ino

--- LED_PUSH_BUTTON_DISPLAY.ino
+++ LED_PUSH_BUTTON_DISPLAY.ino
@@ -1,6 +1,7 @@
 //Alyan Gillett Arduino Projects
 /* Using a push button with digital inputs in order to turn an LED on and off. 
 Pressing the button turns it on, and pressing the other button turns it off. */
+
 int ledPin = 5;
 int buttonApin = 9;
 int buttonBpin = 8;
